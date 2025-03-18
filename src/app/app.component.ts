c417612478c6f48e897d6a6b2895193bd3e032a0
Jerry Phillips <Jerry.Daniel.Phillips@gmail.com>
2025-03-17 21:21:37 -0400

    Add main site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./landing/navigation/navbar/navbar.component";
+import { FooterComponent } from "./landing/navigation/footer/footer.component";
+import { HomeComponent } from "./landing/home/home.component";
+import { PricingComponent } from "./landing/pricing/pricing.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, PricingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
