dab040abf07ee71d27b567a0210752bb9a5c528a
Jerry Phillips <Jerry.Daniel.Phillips@gmail.com>
2025-03-17 21:46:47 -0400

    Fix navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./landing/navigation/navbar/navbar.component";
 import { FooterComponent } from "./landing/navigation/footer/footer.component";
 import { HomeComponent } from "./landing/home/home.component";
 import { PricingComponent } from "./landing/pricing/pricing.component";
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,8 @@ import { PricingComponent } from "./landing/pricing/pricing.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'job-flow-ui-web';
+
+
 }
--- src/app/landing/navigation/navbar/navbar.component.ts
+++ src/app/landing/navigation/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, HostListener, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
+declare var bootstrap: any
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: object, private renderer: Renderer2) {}
 
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('scroll', () => this.onScroll());
+    }
+  }
+
+  onScroll() {
+    const navbar = document.getElementById('navbar');
+    if (navbar) {
+      if (window.scrollY > 50) {
+        this.renderer.addClass(navbar, 'nav-sticky');
+      } else {
+        this.renderer.removeClass(navbar, 'nav-sticky');
+      }
+    }
+  }
 }
