676b431a26a4cbe0ec56521c40a57aec2ba59e78
bllr777 <jerry.daniel.phillips@gmail.com>
2025-03-23 00:03:42 -0400

    Add public vies

--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -1,16 +1,16 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { HomeComponent } from "../../landing/home/home.component";
-import { PricingComponent } from "../../landing/pricing/pricing.component";
 import { RouterOutlet } from '@angular/router';
 import { PublicNavbarComponent } from "./public-navbar/public-navbar.component";
 import { PublicFooterComponent } from "./public-footer/public-footer.component";
 import { BackToTopComponent } from "../../landing/back-to-top.component";
 import { PreloaderComponent } from "../../landing/preloader.component";
+import { HeroComponent } from "../../views/home/components/hero/hero.component";
+import { PricingComponent } from "../../views/home/components/pricing/pricing.component";
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [RouterOutlet, PublicNavbarComponent, PublicFooterComponent, BackToTopComponent, PreloaderComponent],
+  imports: [PublicNavbarComponent, PublicFooterComponent, BackToTopComponent, PreloaderComponent, HeroComponent, PricingComponent],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.scss',
   encapsulation: ViewEncapsulation.None 
---
+++ src/app/views/home/components/pricing/pricing.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { plans } from '../data';
+import { PricingPlan } from '../types';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pricing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.scss'
+})
+export class PricingComponent {
+  plans = plans
+  // Type for the billing toggle (whether it's annual or not)
+  isAnnual: boolean = false;
+
+  // Toggle between monthly and annual billing
+  toggleBilling(): void {
+    this.isAnnual = !this.isAnnual;
+  }
+
+  // Get formatted price depending on billing type (monthly or annual)
+  getPrice(plan: PricingPlan): string {
+    return this.isAnnual ? `$${(plan.price * 12).toFixed(2)} / Year` : `$${plan.price.toFixed(2)} / Month`;
+  }
+}
