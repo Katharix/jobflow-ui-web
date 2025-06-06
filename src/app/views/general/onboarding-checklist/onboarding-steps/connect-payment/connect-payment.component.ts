f7b463a2583dcbfca6a4d3fb2a71cba6eb1ea06a
Jerry Phillips <Jerry.Daniel.Phillips@gmail.com>
2025-06-06 19:19:25 -0400

    Change styling

--- src/app/views/general/onboarding-checklist/onboarding-steps/connect-payment/connect-payment.component.ts
+++ src/app/views/general/onboarding-checklist/onboarding-steps/connect-payment/connect-payment.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { PaymentService } from '../../../../../services/payment.service';
 import { OrganizationContextService } from '../../../../../services/shared/organization-context.service';
+import { PaymentProviders } from '../data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-connect-payment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './connect-payment.component.html',
   styleUrl: './connect-payment.component.scss'
 })
 export class ConnectPaymentComponent implements OnInit {
   orgId: string | undefined;
-
+  paymentProviders = PaymentProviders
   constructor(
     private paymentService: PaymentService,
     private orgContext: OrganizationContextService
---
+++ src/app/views/general/onboarding-checklist/onboarding-steps/data.ts
@@ -0,0 +1,21 @@
+export interface PaymentProvider {
+    logoPath: string;
+    provider: string;
+    description: string;
+    altLogoText: string;
+}
+
+export const PaymentProviders: PaymentProvider[] = [
+    {
+        provider: 'Stripe',
+        logoPath: 'assets/admin/images/photos/StripeLogo.png',
+        description: 'Connect your Stripe account to start accepting online payments directly from your clients.\nIt\'s fast, secure, and essential for invoicing through the platform.',
+        altLogoText: 'Stripe Logo'
+    },
+    {
+        provider: 'Square',
+        logoPath: 'assets/admin/images/photos/Square_LogoLockup_Black.png',
+        description: 'Connect your Square account to start accepting online payments directly from your clients.\nIt\'s quick, secure, and fully integrated with your invoicing workflow.',
+        altLogoText: 'Square Logo'
+    }
+];
