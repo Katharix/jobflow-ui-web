7512c7516bedcb2288cc089fcd7ea1cd2a9919dc
Jerry Phillips <jerry.daniel.phillips@gmail.com>
2026-01-24 13:22:39 -0500

    Auth Changes

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,19 +1,20 @@
-import { Component, ViewEncapsulation, inject } from '@angular/core';
-import { RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet } from '@angular/router';
-import { PreloaderComponent } from "../../landing/preloader.component";
-import { LoadingService } from '../../services/loading-service.service';
-import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import {Component, ViewEncapsulation, inject} from '@angular/core';
+import {RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet} from '@angular/router';
+import {PreloaderComponent} from "../../landing/preloader.component";
+import {LoadingService} from '../../services/loading-service.service';
+import {CommonModule} from '@angular/common';
+import {Observable} from 'rxjs';
+import {LoadingOverlayComponent} from "../../common/app-loading-overlay/app-loading-overlay.component";
 
 @Component({
-  selector: 'app-auth-layout',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, PreloaderComponent],
-  templateUrl: './auth-layout.component.html',
-  styleUrl: './auth-layout.component.scss',
-  encapsulation: ViewEncapsulation.None
+   selector: 'app-auth-layout',
+   standalone: true,
+   imports: [RouterOutlet, CommonModule, PreloaderComponent, LoadingOverlayComponent],
+   templateUrl: './auth-layout.component.html',
+   styleUrl: './auth-layout.component.scss',
+   encapsulation: ViewEncapsulation.None
 })
 export class AuthLayoutComponent {
-  private loadingService = inject(LoadingService);
-  isLoading$: Observable<boolean> = this.loadingService.isLoading$;
+   private loadingService = inject(LoadingService);
+   isLoading$: Observable<boolean> = this.loadingService.isLoading$;
 }
