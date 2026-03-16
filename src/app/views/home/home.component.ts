a2b98254fa73f9e03b109cb5d4c19f51f7f5c9ef
Jerry Phillips <jerry.daniel.phillips@gmail.com>
2026-03-16 18:28:53 -0400

    feat(UIRedesign): New Home UI redesign

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -5,11 +5,12 @@ import { CtaComponent } from "./components/cta/cta.component";
 import { PricingComponent } from "./components/pricing/pricing.component";
 import { ContactComponent } from "./components/contact/contact.component";
 import { AboutComponent } from "./components/about/about.component";
+import { HowItWorksComponent } from "./components/how-it-works/how-it-works.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, FeaturesComponent, CtaComponent, PricingComponent, ContactComponent, AboutComponent],
+  imports: [HeroComponent, FeaturesComponent, CtaComponent, PricingComponent, ContactComponent, AboutComponent, HowItWorksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
